116c520fe85fbbb44fffd8258314571b56fc5d07
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2022-03-09 16:10:36 +0200

    fix: fix migration

--- pkg/bunapp/migrations/20211204231031_initial.down.sql
+++ pkg/bunapp/migrations/20211204231031_initial.down.sql
@@ -1,64 +1,64 @@
 DROP VIEW IF EXISTS span_host_minutes_mv;
 
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS span_host_minutes;
 
---migrate:split
+--migration:split
 
 DROP VIEW IF EXISTS span_host_hours_mv;
 
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS span_host_hours;
 
 --------------------------------------------------------------------------------
---migrate:split
+--migration:split
 
 DROP VIEW IF EXISTS span_service_minutes_mv;
 
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS span_service_minutes;
 
---migrate:split
+--migration:split
 
 DROP VIEW IF EXISTS span_service_hours_mv;
 
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS span_service_hours;
 
 --------------------------------------------------------------------------------
---migrate:split
+--migration:split
 
 DROP VIEW IF EXISTS span_system_minutes_mv;
 
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS span_system_minutes;
 
---migrate:split
+--migration:split
 
 DROP VIEW IF EXISTS span_system_hours_mv;
 
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS span_system_hours;
 
 --------------------------------------------------------------------------------
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS spans_index;
 
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS spans_data;
 
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS spans_index_buffer;
 
---migrate:split
+--migration:split
 
 DROP TABLE IF EXISTS spans_data_buffer;
