ed2d1b9807ed4133f9b1b743e366a62a4f611706
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2022-03-15 16:37:09 +0200

    chore: add metrics stub

---
+++ pkg/tracing/otlp_metrics_grpc.go
@@ -0,0 +1,28 @@
+package tracing
+
+import (
+	"context"
+
+	"github.com/uptrace/uptrace/pkg/bunapp"
+	collectormetricspb "go.opentelemetry.io/proto/otlp/collector/metrics/v1"
+)
+
+type MetricsServiceServer struct {
+	collectormetricspb.UnimplementedMetricsServiceServer
+
+	*bunapp.App
+}
+
+func NewMetricsServiceServer(app *bunapp.App) *MetricsServiceServer {
+	return &MetricsServiceServer{
+		App: app,
+	}
+}
+
+var _ collectormetricspb.MetricsServiceServer = (*MetricsServiceServer)(nil)
+
+func (s *MetricsServiceServer) Export(
+	ctx context.Context, req *collectormetricspb.ExportMetricsServiceRequest,
+) (*collectormetricspb.ExportMetricsServiceResponse, error) {
+	return &collectormetricspb.ExportMetricsServiceResponse{}, nil
+}
