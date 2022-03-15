9a13cbc91bffadf24db0b90d71a3b0d7385e34f1
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2022-03-15 15:54:57 +0200

    fix: resize chart

--- vue/src/plugins/directives.ts
+++ vue/src/plugins/directives.ts
@@ -1,4 +1,6 @@
 import Vue from 'vue'
 import autowidth from '@/directives/autowidth'
+import resize from '@/directives/resize'
 
 Vue.directive('autowidth', autowidth)
+Vue.directive('element-resize', resize)
