35ace51e589c9f913379659be3d75d8079cca406
Anatolii Mihailenco <anmic.webdev@gmail.com>
2022-04-11 19:42:31 +0300

    fix: add formats to xdate component

--- vue/src/components/XDate.vue
+++ vue/src/components/XDate.vue
@@ -1,6 +1,16 @@
 <script lang="ts">
 import Vue from 'vue'
-import { toDate, datetime } from '@/util/date'
+import {
+  toDate,
+  date,
+  dateShort,
+  time,
+  datetime,
+  datetimeShort,
+  datetimeFull,
+  relative,
+  fromNow,
+} from '@/util/date'
 
 export default Vue.component('XDate', {
   functional: true,
@@ -9,13 +19,51 @@ export default Vue.component('XDate', {
       type: [String, Date],
       required: true,
     },
+    format: {
+      type: String,
+      default: '',
+    },
   },
 
   render(h, { props, data }) {
+    const fmt = formatter(props.format)
+
+    if (!data.attrs) {
+      data.attrs = {}
+    }
+
     const dt = toDate(props.date)
-    return h('span', data, datetime(dt))
+
+    if (fmt !== datetimeFull) {
+      data.attrs.title = datetimeFull(dt)
+    }
+
+    return h('span', data, fmt(dt))
   },
 })
+
+function formatter(format: string) {
+  switch (format) {
+    case '':
+      return datetime
+    case 'time':
+      return time
+    case 'date':
+      return date
+    case 'dateShort':
+      return dateShort
+    case 'short':
+      return datetimeShort
+    case 'full':
+      return datetimeFull
+    case 'relative':
+      return relative
+    case 'from-now':
+      return fromNow
+    default:
+      throw new Error(`unknown format=${format}`)
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
