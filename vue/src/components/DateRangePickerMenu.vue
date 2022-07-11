6fc719bfe005e663dd74e425d08b21abeccb3983
Anatolii Mihailenco <anmic.webdev@gmail.com>
2022-07-11 17:44:57 +0300

    feat: add time period picker

---
+++ vue/src/components/DateRangePickerMenu.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-menu v-model="menu" offset-y transition="slide-x-transition" :close-on-content-click="false">
+    <template #activator="{ on, attrs }">
+      <span class="mr-2">
+        <v-icon small v-bind="attrs" v-on="on">mdi-calendar-blank</v-icon>
+        <v-btn v-if="dateRange.hasNextPeriod" text small class="px-1" v-bind="attrs" v-on="on">
+          <span><XDate :date="dateRange.gte" :format="format" /> - </span>
+          <XDate :date="dateRange.lt" :format="format" />
+        </v-btn>
+      </span>
+    </template>
+    <v-card width="auto">
+      <v-card-text>
+        <v-row no-gutters>
+          <v-col class="pt-3 pr-2">From</v-col>
+          <v-col cols="auto" class="d-flex justify-end">
+            <DateTextInput v-model="gte" />
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col class="pt-3 pr-2">To</v-col>
+          <v-col cols="auto" class="d-flex justify-end">
+            <DateTextInput v-model="lt" />
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn class="primary mr-1" :disabled="!isValid" @click="apply">Apply</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, shallowRef, computed, watch, PropType } from '@vue/composition-api'
+
+// Composables
+import { UseDateRange } from '@/use/date-range'
+
+// Components
+import DateTextInput from '@/components/DateTextInput.vue'
+
+// Utilities
+import { hour } from '@/util/date'
+
+export default defineComponent({
+  name: 'DateRangePickerMenu',
+  components: { DateTextInput },
+
+  props: {
+    dateRange: {
+      type: Object as PropType<UseDateRange>,
+      required: true,
+    },
+    format: {
+      type: String,
+      default: 'short',
+    },
+  },
+
+  setup(props) {
+    const menu = ref(false)
+    const gte = shallowRef(new Date(Date.now() - hour))
+    const lt = shallowRef(new Date())
+
+    const isValid = computed((): boolean => {
+      return gte.value! < lt.value!
+    })
+
+    function apply() {
+      props.dateRange.change(gte.value, lt.value)
+      menu.value = false
+    }
+
+    watch(
+      () => props.dateRange.gte,
+      (date: Date | undefined) => {
+        if (date) {
+          gte.value = date
+        }
+      },
+      { immediate: true },
+    )
+
+    watch(
+      () => props.dateRange.lt,
+      (date: Date | undefined) => {
+        if (date) {
+          lt.value = date
+        }
+      },
+      { immediate: true },
+    )
+
+    return {
+      menu,
+      gte,
+      lt,
+      isValid,
+
+      apply,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue/src/components/DateTextInput.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      transition="slide-x-transition"
+      offset-y
+      min-width="auto"
+    >
+      <template #activator="{ on, attrs }">
+        <v-icon v-bind="attrs" v-on="on">mdi-calendar</v-icon>
+        <v-text-field
+          v-model="formattedDate"
+          style="width: 115px"
+          dense
+          outlined
+          hint="dd-mm-yyyy"
+          persistent-hint
+          class="mx-2 d-inline-block"
+          :rules="rules.date"
+          @blur="onBlur"
+        >
+        </v-text-field>
+      </template>
+      <v-date-picker v-model="datePicker" no-title @input="menu = false"></v-date-picker>
+    </v-menu>
+    <v-text-field
+      v-model="formattedTime"
+      style="width: 75px"
+      outlined
+      dense
+      hint="24 hours"
+      persistent-hint
+      class="d-inline-block"
+      :rules="rules.time"
+      @blur="onBlur"
+    ></v-text-field>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, shallowRef, computed, watch } from '@vue/composition-api'
+
+// Utilities
+import { parse, format, isValid } from 'date-fns'
+import { toLocal } from '@/util/date'
+
+const DATE_FORMAT = 'dd-MM-yyyy'
+const TIME_FORMAT = 'H:mm'
+
+export default defineComponent({
+  name: 'DateTextInput',
+
+  props: {
+    value: {
+      type: Date,
+      required: true,
+    },
+  },
+
+  setup(props, { emit }) {
+    const menu = shallowRef(false)
+    const formattedDate = shallowRef('')
+    const formattedTime = shallowRef('')
+
+    const datePicker = computed({
+      get(): string {
+        return toLocal(props.value).toISOString().substr(0, 10)
+      },
+      set(s: string) {
+        formattedDate.value = format(new Date(s), DATE_FORMAT)
+
+        const date = parseDateTime(`${formattedDate.value} ${formattedTime.value}`)
+        emit('input', date)
+      },
+    })
+
+    watch(
+      () => props.value,
+      (date: Date) => {
+        formattedDate.value = format(date, DATE_FORMAT)
+        formattedTime.value = format(date, TIME_FORMAT)
+      },
+      { immediate: true },
+    )
+
+    const rules = {
+      time: [(v: string) => isValidDateTime(v, TIME_FORMAT) || '24 hours'],
+      date: [(v: string) => isValidDateTime(v, DATE_FORMAT) || 'dd-mm-yyyy'],
+    }
+
+    function isValidDateTime(s: string, format: string): boolean {
+      return isValid(parse(s, format, new Date()))
+    }
+
+    function onBlur() {
+      const date = parseDateTime(`${formattedDate.value} ${formattedTime.value}`)
+      if (!isValid(date)) {
+        return
+      }
+
+      emit('input', date)
+    }
+
+    return {
+      menu,
+      datePicker,
+      formattedDate,
+      formattedTime,
+      rules,
+
+      onBlur,
+    }
+  },
+})
+
+function parseDateTime(s: string) {
+  return parse(s, `${DATE_FORMAT} ${TIME_FORMAT}`, new Date())
+}
+</script>
+
+<style lang="scss" scoped></style>
