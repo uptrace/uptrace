855f36685a3252ee7c542a3e2179e947adb4a1a2
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2022-07-28 09:56:07 +0300

    chore: remove unneded spacing

--- vue/src/components/UptraceLogoLarge.vue
+++ vue/src/components/UptraceLogoLarge.vue
@@ -1,5 +1,5 @@
 <template>
   <router-link class="d-inline-block" v-bind="$attrs" v-on="$listeners">
-    <v-img src="/uptrace/logo-large.svg" max-width="160" width="160" alt="Uptrace" class="shrink" />
+    <v-img src="/uptrace/logo-large.svg" max-width="150" width="150" alt="Uptrace" class="shrink" />
   </router-link>
 </template>
