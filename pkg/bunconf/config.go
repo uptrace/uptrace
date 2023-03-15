63190925e300f059b8d90723a7aa70078c2b26f8
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2023-03-15 15:27:46 +0200

    chore: add user email and avatar

--- pkg/bunconf/config.go
+++ pkg/bunconf/config.go
@@ -129,6 +129,9 @@ func validateUsers(users []User) error {
 		if user.ID == 0 {
 			user.ID = xxhash.Sum64String(user.Username)
 		}
+		if user.Avatar == "" {
+			user.Avatar = user.Gravatar()
+		}
 	}
 
 	return nil
@@ -283,37 +286,6 @@ func (l *Listen) init() error {
 
 type CHTableOverride struct {
 	TTL string `yaml:"ttl"`
-}
-
-type User struct {
-	ID       uint64 `yaml:"id" json:"id"`
-	Username string `yaml:"username" json:"username"`
-	Password string `yaml:"password" json:"-"`
-}
-
-type CloudflareProvider struct {
-	TeamURL  string `yaml:"team_url" json:"team_url"`
-	Audience string `yaml:"audience" json:"audience"`
-}
-
-type OIDCProvider struct {
-	ID           string   `yaml:"id" json:"id"`
-	DisplayName  string   `yaml:"display_name" json:"display_name"`
-	IssuerURL    string   `yaml:"issuer_url" json:"issuer_url"`
-	ClientID     string   `yaml:"client_id" json:"client_id"`
-	ClientSecret string   `yaml:"client_secret" json:"client_secret"`
-	RedirectURL  string   `yaml:"redirect_url" json:"redirect_url"`
-	Scopes       []string `yaml:"scopes" json:"scopes"`
-	Claim        string   `yaml:"claim" json:"claim"`
-}
-
-type Project struct {
-	ID                  uint32   `yaml:"id" json:"id"`
-	Name                string   `yaml:"name" json:"name"`
-	Token               string   `yaml:"token" json:"token"`
-	PinnedAttrs         []string `yaml:"pinned_attrs" json:"pinnedAttrs"`
-	GroupByEnv          bool     `yaml:"group_by_env" json:"groupByEnv"`
-	GroupFuncsByService bool     `yaml:"group_funcs_by_service" json:"groupFuncsByService"`
 }
 
 func (c *Config) GRPCEndpoint() string {
