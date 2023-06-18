540d2ee1cad6b48763cb5ba69371fa02e2bc0737
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2023-06-18 10:14:36 +0300

    chore: fix health check

--- internal/packaging/fpm/preuninstall.sh
+++ internal/packaging/fpm/preuninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 if command -v systemctl >/dev/null 2>&1; then
-    systemctl stop uptrace.service
-    systemctl disable uptrace.service
+    systemctl stop uptrace.service || true
+    systemctl disable uptrace.service || true
 fi
