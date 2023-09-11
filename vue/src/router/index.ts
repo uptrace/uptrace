d05dbf17a71f3bcb33cca1a07d94b40a969c0133
Anatolii Mihailenco <anmic.webdev@gmail.com>
2023-09-11 15:09:05 +0300

    chore: cleanup

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -5,7 +5,6 @@ import VueRouter, { RouteConfig, NavigationGuard } from 'vue-router'
 import { useUser } from '@/org/use-users'
 
 import NotFoundPage from '@/org/views/NotFoundPage.vue'
-import ClickHouseTimeoutPage from '@/org/views/ClickHouseTimeoutPage.vue'
 import ProjectSettings from '@/org/views/ProjectSettings.vue'
 
 import Alerting from '@/alerting/views/Alerting.vue'
@@ -299,11 +298,6 @@ const routes: RouteConfig[] = [
     beforeEnter: redirectToProject('MetricsDashList'),
   },
 
-  {
-    path: '/timeout',
-    name: 'ClickHouseTimeoutPage',
-    component: ClickHouseTimeoutPage,
-  },
   { path: '*', component: NotFoundPage },
 ]
 
