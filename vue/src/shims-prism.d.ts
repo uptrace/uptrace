2cceab9e21f8b72990e3570276ba911a557bdfa1
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2024-02-24 16:09:22 +0200

    chore: restore the prism shim

---
+++ vue/src/shims-prism.d.ts
@@ -0,0 +1,8 @@
+declare module 'vue-prism-component' {
+  import { FunctionalComponentOptions } from 'vue'
+  const _default: FunctionalComponentOptions<{
+    code: string
+    language: string
+  }>
+  export default _default
+}
