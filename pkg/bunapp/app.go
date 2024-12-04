0265f71b3e2db3e46bf8d72bcce40bf9f54f4a8e
Yuri Pimenov <yuri.pimenov@gmail.com>
2024-12-04 14:07:26 +0200

    tidy up

--- pkg/bunapp/app.go
+++ pkg/bunapp/app.go
@@ -179,15 +179,6 @@ func (app *App) Zap(ctx context.Context) otelzap.LoggerWithCtx {
 	return app.Logger.Ctx(ctx)
 }
 
-func (app *App) RouterParams() *RouterParams {
-	return &RouterParams{
-		Router:           app.router,
-		RouterGroup:      app.routerGroup,
-		RouterInternalV1: app.internalAPIV1,
-		RouterPublicV1:   app.publicAPIV1,
-	}
-}
-
 //------------------------------------------------------------------------------
 
 func (app *App) initRouter() {
