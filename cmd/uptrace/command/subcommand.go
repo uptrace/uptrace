f862c0994236a260f0ffacfafca35ea769537bf7
Yuri Pimenov <yuri.pimenov@gmail.com>
2024-12-12 01:59:05 +0200

    add forgotten source file

---
+++ cmd/uptrace/command/subcommand.go
@@ -0,0 +1,22 @@
+package command
+
+import (
+	"github.com/urfave/cli/v2"
+	"go.uber.org/fx"
+
+	"github.com/uptrace/uptrace/pkg/bunapp"
+)
+
+func runSubcommand(c *cli.Context, f any, opts ...fx.Option) error {
+	opts = append(opts, fx.Supply(c), fx.NopLogger, fx.Invoke(f))
+	app, err := bunapp.NewApp(
+		c.String("config"),
+		opts...,
+	)
+	if err != nil {
+		return err
+	}
+
+	_ = app.Start(c.Context)
+	return app.Stop(c.Context)
+}
