9621d53eeaf3845ab6aaa19f555c0ba16afe1333
Yuri Pimenov <yuri.pimenov.ext@simplesurance.de>
2025-01-15 16:11:02 +0200

    done

--- pkg/bunapp/pgmigrations/20231221122003_grafana_compat.tx.up.sql
+++ pkg/bunapp/pgmigrations/20231221122003_grafana_compat.tx.up.sql
@@ -1,7 +1,2 @@
-alter table projects
-add column prom_compat boolean not null default false;
-
---bun:split
-
 update grid_items
 set width = 2 * width, x_axis = 2 * x_axis;
--- pkg/bunapp/pgmigrations/20240202182031_add_force_span_name.up.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE projects
-ADD COLUMN force_span_name text[];
-
---bun:split
-
-alter table users
-add column auth_token varchar(500);
